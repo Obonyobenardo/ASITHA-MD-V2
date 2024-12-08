860e446d9432730e281e38faf7e429e573c084b8
Obonyobenardo <151940477+Obonyobenardo@users.noreply.github.com>
2024-12-08 14:23:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WnIkUawb#LsdbAfARfd-m42e13See0VKgpBRumvwfRz4ZLs4sI3U' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=a2YUCJSC#DIlVb1Z9TJEz61U0Qp3ofpKDxNU8TpQmf8RftBzFPbk' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS,
